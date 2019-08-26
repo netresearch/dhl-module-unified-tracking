5af2e31ff6eb898eb95c3adb1bd4148b7abb7d28
Muhammad Qasim <muhammad.qasim@netresearch.de>
2019-08-26 15:44:52 +0200

    DHLGW-492: Integration Test Implement

---
+++ Test/Integration/TestCase/Model/Config/ModuleConfigTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * See LICENSE.md for license details.
+ */
+declare(strict_types=1);
+
+namespace Dhl\GroupTracking\Model\Config;
+
+use http\Exception\UnexpectedValueException;
+use Magento\TestFramework\ObjectManager;
+
+/**
+ * Class ModuleConfigTest
+ *
+ * @package Dhl\GroupTracking\Test\Integration
+ * @author Muhammad Qasim <muhammad.qasim@netresearch.de>
+ * @link   https://www.netresearch.de/
+ */
+class ModuleConfigTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     * @magentoConfigFixture default/dhlshippingsolutions/tracking/consumer_key foo
+     */
+    public function getConsumerKey()
+    {
+        $objectManager = ObjectManager::getInstance();
+        $config = $objectManager->create(ModuleConfig::class);
+        $result = $config->getConsumerKey();
+        self::assertSame('foo', $result);
+    }
+}
