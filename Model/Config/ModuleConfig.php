352f7a08e2c17897a34792f08d921c0ecc9a27ef
Muhammad Qasim <muhammad.qasim@netresearch.de>
2019-08-26 15:38:44 +0200

    DHLGW-492: Integration Test Implement

--- Model/Config/ModuleConfig.php
+++ Model/Config/ModuleConfig.php
@@ -1,19 +1,36 @@
 <?php
+/**
+ * See LICENSE.md for license details.
+ */
 declare(strict_types=1);
 
-namespace Dhl\Grouptracking\Model\Config;
+namespace Dhl\GroupTracking\Model\Config;
 
 use Magento\Framework\App\Config\ScopeConfigInterface;
 
+/**
+ * Class ModuleConfig
+ *
+ * @package Dhl\GroupTracking\Model
+ * @author Muhammad Qasim <muhammad.qasim@netresearch.de>
+ * @link   https://www.netresearch.de/
+ */
 class ModuleConfig
 {
     const CONFIG_PATH_CONSUMER_KEY = 'dhlshippingsolutions/tracking/consumer_key';
 
+    /**
+     * ModuleConfig constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
     public function __construct(ScopeConfigInterface $scopeConfig)
     {
         $this->scopeConfig = $scopeConfig;
     }
 
+    /**
+     * @return string
+     */
     public function getConsumerKey(): string
     {
         return $this->scopeConfig->getValue(self::CONFIG_PATH_CONSUMER_KEY);
